13d4bb6fa5c4d6ff968a1a235ba2a163009c2716
mt-codes-eng <mai.tooyama@gmail.com>
2026-01-31 01:25:22 +0900

    accounts: signup ビューでフォームを表示

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 def login_view(request):
     return render(request, "accounts/login.html")
 
 def signup_view(request):
-    return render(request, "accounts/signup.html")
+    form = SignUpForm() # その型から作られた実物（インスタンス）。まだ何も書かれていない入力用の紙を1枚用意した
+    # {"form": form}でその紙をHTMLに渡して、『ここに表示してね』と頼んだ。テンプレート側で form を使えるように。辞書の意味：左 "form" → HTMLで使う名前、右 form → Pythonで作った申込書そのもの。
+    return render(request, "accounts/signup.html", {"form": form}) # Python的に省略していない形はreturn render(request=request,template_name="accounts/signup.html",context={"form": form})
