1f4c6b3f31055aeeb2f936a55246b0d5c39351cf
mt-codes-eng <mai.tooyama@gmail.com>
2026-01-30 22:30:55 +0900

    accounts: メールアドレス認証用のカスタムUserモデルを定義

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = None # username（ユーザー名）を使わず、emailをログインIDにしたいので消す。Django標準Userが持つ username 欄を 使わない宣言
+    email = models.EmailField("メールアドレス", unique=True) # email をログインIDとして使う（unique=True で重複登録を禁止）
+    USERNAME_FIELD = "email" # Djangoに「ログインIDはemailだよ」と教える
+    REQUIRED_FIELDS = [] # createsuperuser を作るときに USERNAME_FIELD 以外で「必須として聞く項目」を指定するリスト。createsuperuser 実行時に Email と Passwordだけ聞かれる
