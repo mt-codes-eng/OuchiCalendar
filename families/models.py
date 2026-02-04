303a3a09d9982849349e3f4650edb42adf299987
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-04 22:13:07 +0900

    families: Familyモデルを追加

--- families/models.py
+++ families/models.py
@@ -1,3 +1,13 @@
+# families/models.py
 from django.db import models
 
-# Create your models here.
+class Family(models.Model):
+    name = models.CharField("苗字", max_length=30, blank=True) # フォーム入力で空を許可,Python・画面側の話
+    image_url = models.CharField("家族アイコンURL", max_length=300, blank=True)
+
+    created_at = models.DateTimeField("作成日時", auto_now_add=True) # 作った瞬間の日時を自動保存
+    updated_at = models.DateTimeField("更新日時", auto_now=True) # 更新するたびに自動更新
+
+    def __str__(self):
+        # 管理画面などで表示される文字
+        return self.name or f"Family {self.id}" # nameがあればそれを表示 or まだ未設定ならFamily1のように表示
