f4e6a467f6f5165c74dbeb7bb030cfd65250e0b6
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-05 00:04:46 +0900

    families: Familyモデルの表示名を整理

--- families/models.py
+++ families/models.py
@@ -2,11 +2,11 @@
 from django.db import models
 
 class Family(models.Model):
-    name = models.CharField("苗字", max_length=30, blank=True) # フォーム入力で空を許可,Python・画面側の話
-    image_url = models.CharField("家族アイコンURL", max_length=300, blank=True)
+    name = models.CharField(verbose_name="家族名", max_length=30, blank=True) # フォーム入力で空を許可,Python・画面側の話
+    image_url = models.CharField(verbose_name="家族アイコン", max_length=300, blank=True)
 
-    created_at = models.DateTimeField("作成日時", auto_now_add=True) # 作った瞬間の日時を自動保存
-    updated_at = models.DateTimeField("更新日時", auto_now=True) # 更新するたびに自動更新
+    created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True) # 作った瞬間の日時を自動保存
+    updated_at = models.DateTimeField(verbose_name="更新日時", auto_now=True) # 更新するたびに自動更新
 
     def __str__(self):
         # 管理画面などで表示される文字
