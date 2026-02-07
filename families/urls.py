998e77b4d99dd6a5805533d9035f7333a70839f3
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-08 01:07:42 +0900

    families: family_profile_edit用URLを追加

--- families/urls.py
+++ families/urls.py
@@ -5,4 +5,5 @@ app_name = "families"
 
 urlpatterns = [
     path("family-settings/", views.family_settings_view, name="family_settings"),
+    path("family-profile/edit/", views.family_profile_edit_view, name="family_profile_edit"),
 ]
