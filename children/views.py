7ec3b01092f0d17ba72e3982c15040cc093e3bd5
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-11 00:38:50 +0900

    children: child_listビューを本実装に修正

--- children/views.py
+++ children/views.py
@@ -1,10 +1,18 @@
+# children/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Child
 
 @login_required
 def child_list_view(request):
-    return HttpResponse("child_list")
+    # クエリ（DB検索）を実行
+    # Child.objects → Childテーブルを操作するための入口
+    # .filter(...) → 条件をつけて絞り込み
+    # .order_by("id") → 並び順を指定
+    # family が request.user.family の子どもだけに絞る = ログイン中のユーザーの家族に属する子どもだけを取得する
+    children = Child.objects.filter(family=request.user.family).order_by("id")
+    return render(request, "children/child_list.html", {"children": children})
 
 @login_required
 def child_create_view(request):
