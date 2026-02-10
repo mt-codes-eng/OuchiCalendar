6e0c22d47b54edfe5ddf15c0e753b6d379d44cb5
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-10 23:12:31 +0900

    children: ルーティング確認用にビューを追加

--- children/views.py
+++ children/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def child_list_view(request):
+    return HttpResponse("child_list")
+
+@login_required
+def child_create_view(request):
+    return HttpResponse("child_create")
+
+@login_required
+def child_edit_view(request, pk):
+    return HttpResponse(f"child_edit {pk}")
+
+@login_required
+def child_delete_view(request, pk):
+    return HttpResponse(f"child_delete {pk}")
