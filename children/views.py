a569f15133008739f5660b3f43b667ea4aaa0a1e
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-12 01:10:47 +0900

    children: child_createビューを本実装に修正

--- children/views.py
+++ children/views.py
@@ -1,8 +1,9 @@
 # children/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Child
+from .forms import ChildForm
 
 @login_required
 def child_list_view(request):
@@ -18,7 +19,24 @@ def child_list_view(request):
 
 @login_required
 def child_create_view(request):
-    return HttpResponse("child_create")
+    if request.method == "POST":
+        # 送信されたデータ（記入済みの紙）でフォームを作る
+        form = ChildForm(request.POST)
+        
+        if form.is_valid():
+            # フォームに入っているデータを使って、Childオブジェクトを作る
+            # Child(name="たろう", image_url="aaa")というようなChild型のオブジェクトをPythonの中で作る。
+            child = form.save(commit=False)
+            # 作ったChildオブジェクトに family を後から代入している
+            # ログイン中のユーザーが所属している家族をchildren_childテーブルのfamily欄に入れる
+            child.family = request.user.family
+            child.save()
+            return redirect("children:child_list")
+        
+    else:
+        form = ChildForm()
+        
+    return render(request, "children/child_form.html", {"form": form})
 
 @login_required
 def child_edit_view(request, pk):
