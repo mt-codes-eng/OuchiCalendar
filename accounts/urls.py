bc43a537eee624808acd52f75412e99f388fc541
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-13 22:45:06 +0900

    accounts: password_changeのURLを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("login/", views.login_view, name="login"),
-    path("signup/", views.signup_view, name="signup"), 
+    path("signup/", views.signup_view, name="signup"),
+    path("password/change", views.password_change_view, name="password_change"), 
 ]
