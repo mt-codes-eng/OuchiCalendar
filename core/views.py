283739d9ef2ae7794603c9c8b21d4c1300b6ed4b
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-15 22:24:19 +0900

    core: settings(設定画面)とlogoutのビューを作成

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def settings_view(request):
+    return render(request, "core/settings.html")
+
+@login_required
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("accounts:login")
+    return render(request, "core/logout_confirm.html")
+
+    
+
