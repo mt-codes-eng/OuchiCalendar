eac2880cf32a97ef934895839e546f47b45cb322
mt-codes-eng <mai.tooyama@gmail.com>
2026-02-17 23:56:59 +0900

    accounts: user_profile_editのURLを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("signup/", views.signup_view, name="signup"),
     path("password/change", views.password_change_view, name="password_change"), 
+    path("user-profile/edit/", views.user_profile_edit_view, name="user_profile_edit"),
 ]
