abd6233ccc5460683b16fa4d1c9807ec294b57fd
mt-codes-eng <mai.tooyama@gmail.com>
2026-03-20 22:32:32 +0900

    children: 画像アイコン項目追加にともないChildモデルとChildFormを修正

--- children/forms.py
+++ children/forms.py
@@ -5,4 +5,18 @@ from .models import Child
 class ChildForm(forms.ModelForm):
     class Meta:
         model = Child
-        fields = ["name", "image_url"]
+        fields = ["name", "image"]
+        labels = {
+            "name": "子ども名",
+            "image": "子どもアイコン",
+        }
+        widgets = {
+            "image": forms.FileInput(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # 編集時に画像未選択でも、既存画像をそのまま維持できるようにする
+        if self.instance and self.instance.pk:
+            self.fields["image"].required = False
