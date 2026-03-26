2db14ef9839d608651ee017ae59d556a25c004de
mt-codes-eng <mai.tooyama@gmail.com>
2026-03-26 23:37:54 +0900

    color_assignments: 色表示用のconstants.pyを作成

---
+++ color_assignments/constants.py
@@ -0,0 +1,19 @@
+# color_assignments/constants.py
+
+# color_code（0〜12）と、実際のHEXカラーを対応づける辞書
+# 画面で色を表示するときに使う
+COLOR_HEX_MAP = {
+    0: "#ff7f7f",
+    1: "#ff7fbf",
+    2: "#ffbcff",
+    3: "#bf7fff",
+    4: "#7f7fff",
+    5: "#7fbfff",
+    6: "#7fffff",
+    7: "#99ffcc",
+    8: "#7fff7f",
+    9: "#bfff7f",
+    10: "#ffff7f",
+    11: "#ffbf7f",
+    12: "#c2bbb2",
+}
