2b9724b1dd11ca677fd2f47206bfead195bf1977
mt-codes-eng <mai.tooyama@gmail.com>
2026-04-02 23:18:55 +0900

    families: setup_completedビューのURLを追加

--- families/urls.py
+++ families/urls.py
@@ -6,4 +6,5 @@ app_name = "families"
 urlpatterns = [
     path("family-settings/", views.family_settings_view, name="family_settings"),
     path("family-profile/edit/", views.family_profile_edit_view, name="family_profile_edit"),
+    path("setup-completed/", views.setup_completed_view, name="setup_completed"),
 ]
