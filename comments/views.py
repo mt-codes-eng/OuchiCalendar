3357fd97f7716ae49660c5ada36c9a9f0253d052
mt-codes-eng <mai.tooyama@gmail.com>
2026-04-30 22:42:28 +0900

    comments: comment_recentビューの追加

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import ScheduleComment
 
-# Create your views here.
+@login_required
+def comment_recent_view(request):
+    """
+    コメント新着画面
+    """
+
+    comments = (
+        ScheduleComment.objects
+        .select_related("schedule", "from_user", "to_user", "schedule__user")
+        .filter(
+            # Q(...) | Q(...) | Q(...)：または条件
+            Q(from_user=request.user) | # 自分が投稿したコメント
+            Q(to_user=request.user) | # 自分宛てのコメント
+            Q(schedule__user=request.user) # 自分が担当者の予定についたコメント
+        )
+        .order_by("-created_at")
+        .distinct()[:20] # 最初は20件表示とする
+    )
+
+    context = {
+        "comments": comments,
+    }
+
+    return render(request, "comments/comment_recent.html", context)
