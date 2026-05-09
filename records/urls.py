6854e1939b06410f61fa0fc3d22d11cba49a844d
mt-codes-eng <mai.tooyama@gmail.com>
2026-05-09 23:28:59 +0900

    records: bowel_record_detailビュー/absence_record_detailビューのURLを追加

--- records/urls.py
+++ records/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "records"
 
 urlpatterns = [
-    path("create/", views.record_create_view, name="create"), # 記録作成画面
+    path("create/", views.record_create_view, name="create"), # 記録作成
+    path("bowel/<int:pk>/", views.bowel_record_detail_view, name="bowel_detail"), # 排便記録詳細
+    path("absence/<int:pk>/", views.absence_record_detail_view, name="absence_detail"), # 欠席記録詳細
 ]
