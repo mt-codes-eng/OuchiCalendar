2a4872743e4f1c82f6a7d8c64756e35fbfe6f11d
mt-codes-eng <mai.tooyama@gmail.com>
2026-05-11 22:57:28 +0900

    records: bowel_record_deleteビュー/absence_record_deleteビューのURLを追加

--- records/urls.py
+++ records/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("absence/<int:pk>/", views.absence_record_detail_view, name="absence_detail"), # 欠席記録詳細
     path("bowel/<int:pk>/edit/", views.bowel_record_edit_view, name="bowel_edit"), # 排便記録編集
     path("absence/<int:pk>/edit/", views.absence_record_edit_view, name="absence_edit"), # 欠席記録編集
+    path("bowel/<int:pk>/delete/", views.bowel_record_delete_view, name="bowel_delete"),
+    path("absence/<int:pk>/delete/", views.absence_record_delete_view, name="absence_delete"),
 ]
