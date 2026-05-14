b762a67deed6c35037180c88bbfdc44acd0b2538
mt-codes-eng <mai.tooyama@gmail.com>
2026-05-14 23:51:27 +0900

    search: searchビューを仮作成しルーティング疎通を確認

--- search/views.py
+++ search/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
-# Create your views here.
+@login_required
+def search_view(request):
+    """
+    検索画面
+
+    まずは検索画面を表示するだけ。
+    このあと予定・記録の検索処理を追加していく。
+    """
+
+    context = {
+        "today_str": timezone.localdate().strftime("%Y-%m-%d"),
+    }
+
+    return render(request, "search/search.html", context)
