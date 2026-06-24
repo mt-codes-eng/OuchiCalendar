e2ce75d588b92696e5ffb85abcc6a0ac5e143bae
mt-codes-eng <mai.tooyama@gmail.com>
2026-06-25 01:03:47 +0900

    core: settingsビューの削除、logoutビューのリダイレクト先を修正

--- core/views.py
+++ core/views.py
@@ -2,26 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def settings_view(request):
-    family = request.user.family
-    
-    # どこから来たか取得
-    next_url = request.GET.get("next")
-    
-    context = {
-        "family": family,
-        "next_url": next_url,
-    }
-
-    return render(request, "core/settings.html", context,)
 
 @login_required
 def logout_view(request):
     if request.method == "POST":
         logout(request)
         return redirect("accounts:login")
-    return render(request, "core/logout_confirm.html")
+    
+    return redirect("schedule:month")
 
 @login_required
 def howto_view(request):
